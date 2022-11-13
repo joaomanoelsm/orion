434ed7df58dd39335f7d47648dc5540a80a74f44
João Manoel <joaom.developer@gmail.com>
2022-11-13 11:51:00 -0300

    homepage trocada

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://github.com/joaomanoelsm/orion',
+  base: 'https://joaomanoelsm.github.io/orion/',
   plugins: [react()]
 })
